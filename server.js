7e577864336f1dca09901c8806c9d3055cfc1ca2
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-05-01 23:21:27 +0400

    Integrating db-connection

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 "use strict";
 const express = require("express");
 require("dotenv/config");
+const db_connection = require("./utils/dbConnection");
 const PORT = process.env.SERVER_PORT;
 const HOST = process.env.SERVER_HOST;
 
+//db connection
+db_connection.db_connection;
+//create express app
 const app = express();
 
 //running the server
