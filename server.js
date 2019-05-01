9af4cb28d29ac55953ba7d7c108c0aee2ebfdc7a
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-05-01 22:38:16 +0400

    Server Init

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 "use strict";
+const express = require("express");
+const config = require("dotenv/config");
+const PORT = process.env.PORT || config.SERVER_PORT;
+const HOST = process.env.HOST || config.SERVER_HOST;
+
+const app = express();
+
+//running the server
+app.listen(PORT, () => {
+  console.log(`Server Running at:${HOST}:${PORT}`);
+});
