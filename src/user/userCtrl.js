76e27c2564da421b5207313f5675fc554bc0c728
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-05-06 22:26:02 +0400

    Add user

--- src/user/userCtrl.js
+++ src/user/userCtrl.js
@@ -1,5 +1,30 @@
 "use strict";
 const User = require("./userModel");
 
-class UserController {}
+class UserController {
+  /**
+   * Add new User
+   * @param {Object} user
+   * @return {Object} success,data,msg
+   */
+  create(user) {
+    const addUser = new User(user);
+    return addUser
+      .save()
+      .then(saved => {
+        return {
+          success: 1,
+          msg: "User saved successfully",
+          data: saved
+        };
+      })
+      .catch(err => {
+        return {
+          success: 0,
+          msg: "Error! Details:" + err,
+          data: err
+        };
+      });
+  }
+}
 module.exports = new UserController();
