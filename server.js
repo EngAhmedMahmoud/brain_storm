44fa14d4885f838969559a0e022721b8074fc769
EngAhmed <ahmed.m.web.dev@gmail.com>
2019-05-06 22:43:49 +0400

    Adding users routes

--- server.js
+++ server.js
@@ -2,12 +2,22 @@
 const express = require("express");
 require("dotenv/config");
 const db_connection = require("./utils/dbConnection");
+const bodyParser = require("body-parser");
+const userRoutes = require("./src/user/userRoutes");
 const PORT = process.env.SERVER_PORT;
 const HOST = process.env.SERVER_HOST;
 //db connection
 db_connection.db_connection;
+
 //create express app
 const app = express();
+
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//user routes
+app.use("/users", userRoutes);
 
 //running the server
 app.listen(PORT, () => {
